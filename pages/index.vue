6fb6d0b59b2584d80c29acefc1e53a01266db2bc
Jakub Jelínek <jakubkoje@gmail.com>
2024-05-20 21:47:39 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+        <h1 @click="navigateTo('/test')">Home</h1>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/test.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+        <h1 @click="navigateTo('/')">Test</h1>
+
+    </div>
+</template>
+
+<script setup lang="ts"></script>
